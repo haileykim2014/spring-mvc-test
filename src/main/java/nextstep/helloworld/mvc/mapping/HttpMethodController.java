66412226f4f2cc24f0881cdf62effce74ed4af50
haileykim2014 <haileykim2014@gmail.com>
2023-05-13 15:37:13 +0900

    RequestMapping

--- src/main/java/nextstep/helloworld/mvc/mapping/HttpMethodController.java
+++ src/main/java/nextstep/helloworld/mvc/mapping/HttpMethodController.java
@@ -1,6 +1,7 @@
 package nextstep.helloworld.mvc.mapping;
 
 import nextstep.helloworld.mvc.domain.User;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,13 +10,16 @@ import java.util.Arrays;
 import java.util.List;
 
 @RestController
+@RequestMapping("/http-method")
 public class HttpMethodController {
 
+    @PostMapping(value = "/users",produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity createUser(@RequestBody User user) {
         Long id = 1L;
         return ResponseEntity.created(URI.create("/users/" + id)).build();
     }
 
+    @GetMapping(value = "/users",produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<User>> showUser() {
         List<User> users = Arrays.asList(
                 new User("이름", "email"),
